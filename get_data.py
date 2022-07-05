f53742a56737d75353ad15124b971e06a15c976d
calluume <cdevans98@gmail.com>
2022-07-05 18:25:21 +0100

    Improved Example & Summary Files

--- get_data.py
+++ get_data.py
@@ -2,12 +2,46 @@ import requests, math, json
 
 import geopandas as gpd
 import matplotlib.pyplot as plt
-import xml.etree.ElementTree as ET
 
 from tqdm import tqdm
 from time import sleep
 from geopy.distance import geodesic
 from os import remove, path, makedirs
+
+def create_summary_file(output_file, bounds, divisions, print_output=True):
+    """
+    Creates a summary MD file with information about bounds & divisions.
+    :param output_file: Name of GEOJSON grid output file
+    :param bounds: Coordinates of 2 diagonally opposite corners of bounds
+    :param divisions: (2x3) array detailing divisions
+    :param print_output: Print summary file to console
+    """
+
+    filepath, _ = path.split(output_file)
+    subdirectory = filepath
+
+    if filepath == '.': title, filepath = '# Data Summary', 'summary.txt'
+    else:
+        dataset_name = filepath.split('/')[-1]
+        title, filepath = '# Summary: ' + dataset_name, dataset_name + '_summary.md'
+
+    with open(subdirectory+'/'+filepath, 'w') as summary_file:
+        summary_file.write(title)
+    
+        max_x, min_x = max([coor[0] for coor in bounds]), min([coor[0] for coor in bounds])
+        max_y, min_y = max([coor[1] for coor in bounds]), min([coor[1] for coor in bounds])
+        summary_file.write("\n\n## Bounds:\n - Latitude: {0} -> {1}\n - Longitude: {2} -> {3}".format(min_x, max_x, min_y, max_y))
+        
+        y_divisions, x_divisions = divisions[0][0], divisions[1][0]
+        summary_file.write("\n - Height: {0}m\n - Width: {1}m".format(round(divisions[0][-1] * y_divisions, 2), round(divisions[1][-1] * x_divisions, 2)))
+
+        summary_file.write("\n\n## Divisions:\n - {0} regions ({1}x{2})".format(x_divisions * y_divisions, x_divisions, y_divisions))  
+        summary_file.write("\n - Height: {0}m\n - Width: {1}m".format(round(divisions[0][-1], 2), round(divisions[1][-1], 2)))
+    
+    if print_output:
+        with open(subdirectory+'/'+filepath, 'r') as file:
+            for line in file: print(line.replace('\n', ''))
+        print()
 
 def generate_geojson_grid(bounds, min_division=100, num_divisions=None, output_file='grid.geojson'):
     """
@@ -30,6 +64,7 @@ def generate_geojson_grid(bounds, min_division=100, num_divisions=None, output_f
 
     divisions = []
     if num_divisions == None:
+        # Calculate number & dimensions of smallest possible divisions
         for dimension, diff in zip([width, height], [max_x - min_x, max_y - min_y]):
             division_size = min_division
             num_divisions = dimension / division_size
@@ -41,6 +76,9 @@ def generate_geojson_grid(bounds, min_division=100, num_divisions=None, output_f
         for dim_division, dimension, diff in zip(num_divisions, [width, height], [max_x - min_x, max_y - min_y]):
             divisions.append([dim_division, diff / dim_division, dimension / dim_division])
 
+    create_summary_file(output_file, bounds, divisions)
+
+    # Save each grid division in GEOJSON format
     geojson_grid = {"type":"FeatureCollection","features":[]}
     current_grid_coors = [min_x, min_y]
     for _ in range(divisions[0][0]):
@@ -76,7 +114,7 @@ def visualise_gpd_data(geojson_file, data_key='population'):
     gpd_data.plot(column=data_key, ax=ax, legend=True)
     plt.show(block=True)
 
-def get_worldpop_data(geojson_file, year=2010, output_file=None, delete_original=True):
+def get_worldpop_data(geojson_file, year=2010, output_file='pop.geojson', delete_original=True):
     """
     Fetches the WorldPop population data for each feature in a GEOJSON file.
     :param geojson_file: GEOJSON file to add data for
@@ -89,23 +127,29 @@ def get_worldpop_data(geojson_file, year=2010, output_file=None, delete_original
         geojson_data = json.load(f)
 
     print('Fetching population:\n  ↳ {0} regions\n'.format(len(geojson_data['features'])))
-
+    
+    total_population = 0
     pbar = tqdm(geojson_data['features'])
     for grid in pbar:
         str_feature = json.dumps(grid)
         str_feature = '{"type":"FeatureCollection","features":['+str_feature+']}'
         
+        # Request population data for each grid in the GEOJSON file (done synchronously)
         pbar.set_description("Sending initial request")
         pop_request_url = "https://api.worldpop.org/v1/services/stats?dataset=wpgppop&year={0}&geojson={1}&runasync=false".format(year, str_feature)
         request_response = requests.get(pop_request_url).json()
         status, is_error = request_response['status'], request_response['error']
 
+        # If successful, add the population to the GEOJSON file
         if 'data' in request_response.keys() and not is_error:
             grid_population = request_response['data']['total_population']
 
+        # If the task takes longer than 30s, we treat it as asynchronous and monitor
+        # the request until the data is collected
         elif status in ['created', 'started', 'finished'] and not is_error:
             pbar.set_description("Fetching data from task id")
             
+            # Get task ID of request to monitor status
             task_id = request_response['taskid']
 
             fetched, current_timeout = False, 10
@@ -114,26 +158,37 @@ def get_worldpop_data(geojson_file, year=2010, output_file=None, delete_original
                 status_request_url = 'https://api.worldpop.org/v1/tasks/' + task_id
                 status_response = requests.get(status_request_url).json()
 
+                # If successful, add the population to the GEOJSON file
                 if 'data' in status_response.keys() and not status_response['error']:
                     fetched, grid_population = True, status_response['data']['total_population']
 
+                # If still no error, but the task is not finished, timeout and increase timeout
+                # for next iteration
                 elif status_response['status'] != 'finished' and not status_response['error']:
                     pbar.set_description("Unsuccessful, waiting {0}".format(current_timeout))
                     sleep(current_timeout)
                     current_timeout += 10
                 
                 elif status_response['error']:
-                    print('\nError:\n'+status_response['error_message'])
+                    print('\nError:\n - '+status_response['error_message'])
                     exit()
-
+        else:
+            print('\nError:\n - '+request_response['error_description'])
+            exit()
         grid['properties']['population'] = grid_population
+        total_population += grid_population
     
-    if output_file == None: output_file = geojson_file[:-8] + '_pop.geojson'
     print("Fetched population data!\n  ↳ Saving to : '{0}'".format(output_file))
     with open(output_file, 'w') as output:
         json.dump(geojson_data, output, indent=4)
 
     if delete_original: remove(geojson_file)
+
+    filepath, _ = path.split(output_file)
+    summary_file = filepath + '/' + filepath.split('/')[-1] + '_summary.md'
+    if path.exists(summary_file):
+        with open(summary_file, 'a') as f:
+            f.write("\n\n## Population Statistics:\n - Total: {0}\n - Average: {1}".format(total_population, round(total_population / len(geojson_data['features']), 2)))
 
 def get_road_layout(bounds, output_file='roads.xml'):
     """
@@ -146,6 +201,8 @@ def get_road_layout(bounds, output_file='roads.xml'):
         max_x, min_x = max([coor[1] for coor in bounds]), min([coor[1] for coor in bounds])
         max_y, min_y = max([coor[0] for coor in bounds]), min([coor[0] for coor in bounds])
 
+    # Fetches all OSM ways (and their nodes) from within the bounds
+    # Only gets ways with 'name' and 'highway' tags, ignores ways classed as areas (eg. some plazas etc)
     overpass_query = "[out:xml];way({0},{1},{2},{3})['name']['highway']['area'!~'yes'];(._;>;);out;".format(min_y, min_x, max_y, max_x)
     
     overpass_url = "http://overpass-api.de/api/interpreter"
@@ -164,8 +221,8 @@ if __name__ == "__main__":
 
     grid_bounds = [[50.737069, -3.559872], [50.704257, -3.491951]]
 
-    generate_geojson_grid(bounds=grid_bounds, min_division=200, output_file=grid_geojson_file)
+    generate_geojson_grid(bounds=grid_bounds, min_division=100, output_file=grid_geojson_file)
     get_worldpop_data(grid_geojson_file, output_file=pop_geojson_file, delete_original=False)
     get_road_layout(grid_bounds, roads_xml_file)
 
-    #visualise_gpd_data(pop_geojson_file)
+    visualise_gpd_data(pop_geojson_file)
---
+++ results/exeter_data/exeter_data_summary.md
@@ -0,0 +1,16 @@
+# Summary: exeter_data
+
+## Bounds:
+ - Latitude: 50.704257 -> 50.737069
+ - Longitude: -3.559872 -> -3.491951
+ - Height: 3650.1m
+ - Width: 4798.14m
+
+## Divisions:
+ - 1692 regions (47x36)
+ - Height: 101.39m
+ - Width: 102.09m
+
+## Population Statistics:
+ - Total: 79053.93
+ - Average: 46.72
